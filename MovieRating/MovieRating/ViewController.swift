64e62afd51c7ef88a6e68c342d44238d44d31f9a
Victor Winbringer <victorwinbringer@MacBook-Pro-Victor.local>
2019-03-16 19:40:49 +0300

    added RxSwift

---
+++ MovieRating/MovieRating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieRating
+//
+//  Created by Victor Winbringer on 15/03/2019.
+//  Copyright © 2019 Victor Winbringer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
